244cdf6237eea14b7013a261e8c94a6b5eae878b
Josep <josep.recio@outlook.com>
2018-02-20 10:46:02 +0100

    Arreglos de la branca

--- src/main/resources/static/js/tasks_actions.js
+++ src/main/resources/static/js/tasks_actions.js
@@ -25,13 +25,8 @@ $(document).ready(function(){
 								}
 								
 								if($("#userassigned_" + idTask).val() != undefined){
-<<<<<<< HEAD
-									$('#editBacklogTaskUser').val($("#userassigned_" + idTask).val());
-									$('#editBacklogTaskUser').selectpicker('refresh');
-=======
                                     $('#editBacklogTaskUser').val($("#userassigned_" + idTask).val());
                                     $('#editBacklogTaskUser').selectpicker('refresh');
->>>>>>> josep5
 								}
 								
 							},
